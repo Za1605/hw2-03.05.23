d8b65aee92516c382092e06608a358b0b578dc09
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-06-23 23:30:04 +0300

    seven commit

--- 03.05.js
+++ 03.05.js
@@ -42,11 +42,13 @@ const box6 = {
     title: `book5`,
     pageCount: 200,
     genre: `rom`,
-    autors: [{name: `Yuliia`, age: 29},
+    autors: [
+        {name: `Yuliia`, age: 29},
         {name: `Petro`, age: 37},
-    ],
 
-}
+    ]};
+
+
 
 console.log(box6.autors[0].age);
 
@@ -57,7 +59,7 @@ console.log(box6.autors[0].age);
 //Створити масив з 10 об'єктами які описують сутніть "користувач".' +
  //Поля: name, username,password. Вивести в консоль пароль кожного користувача
 
-const user = [
+const users = [
 
     {name: `Petro`, username: `Svanivach`, password:123},
     {name: `Sty`, username: `Fil`, password:124},
@@ -70,7 +72,45 @@ const user = [
     {name: `Kel`, username: `Sva`, password:145},
     {name: `Din`, username: `Chiy`, password:156},
 ];
-console.log(user[0].password);
+console.log(users.password);
 
 
+//Логічні розгалуження:
+    //Є змінна х, якій ви надаєте довільне числове значення.
+    //Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
 
+ let x = -3;
+ if (x != 0)
+ {
+ console.log(`Вірно`);}
+
+ else{
+     console.log('Невірно');}
+
+
+//Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
+//(в першу, другу, третю или четверту частину години).
+
+let time = 45;
+
+ if (time >=0 && time <15 ) {
+     console.log(`first part`);
+ }
+
+ else if (time >=15 && time < 30){
+     console.log(`second part`);
+ }
+
+ else if
+ (time >= 30 && time < 45){
+     console.log( `third part`);
+ }
+
+ else if (time >= 45 && time <60){
+     console.log(`fourth part`);
+ }
+ else {
+     console.log(`undefind`);
+ }
+
+//У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
